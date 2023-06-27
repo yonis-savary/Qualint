e298d359d21d9836ac53bee6ea5caf1a5160b216
YonisSavary <yonissavary@outlook.fr>
2023-06-27 16:48:41 +0200

    Fixed code format

--- src/Norms/ValidUses.php
+++ src/Norms/ValidUses.php
@@ -27,9 +27,6 @@ class ValidUses extends AbstractNorm
 
             $isUsed = count($matches[0]) > 1;
 
-            if (class_exists($class) && $isUsed)
-                continue;
-
             if (!$isUsed)
             {
                 $this->parent->mutate(
@@ -41,38 +38,37 @@ class ValidUses extends AbstractNorm
                 continue;
             }
 
-            if (!class_exists($class))
+            if (class_exists($class))
+                continue;
+
+            $found = false;
+            foreach (get_declared_classes() as $possibleClass)
             {
-                $found = false;
-                foreach (get_declared_classes() as $possibleClass)
-                {
-                    if (!str_ends_with($possibleClass, $classname))
-                        continue;
-
-                    $replacement = "use $possibleClass";
-                    if ($alias) $replacement .= " as $alias";
-                    $replacement .=";\n";
-
-                    $this->parent->mutate(
-                        "Fixing bad use statement [$class] to [$possibleClass]",
-                        fn(AnalysisText $text) => $text->replaceLines($line, $line, $replacement),
-                        line: $line
-                    );
-
-                    $found = true;
-                    break;
-                }
-                if ($found)
+                if (!str_ends_with($possibleClass, $classname))
                     continue;
 
+                $replacement = "use $possibleClass";
+                if ($alias) $replacement .= " as $alias";
+                $replacement .=";\n";
+
                 $this->parent->mutate(
-                    'Removing bad use statement',
-                    fn(AnalysisText $text) => $text->replaceLines($line, $line, ''),
+                    "Fixing bad use statement [$class] to [$possibleClass]",
+                    fn(AnalysisText $text) => $text->replaceLines($line, $line, $replacement),
                     line: $line
                 );
-                $i--;
-                continue;
+
+                $found = true;
+                break;
             }
+            if ($found)
+                continue;
+
+            $this->parent->mutate(
+                'Removing bad use statement',
+                fn(AnalysisText $text) => $text->replaceLines($line, $line, ''),
+                line: $line
+            );
+            $i--;
         }
     }
 }
